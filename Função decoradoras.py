a746bc3f9bb1a9c1450ca91f0e798a997cfb60f2
Adriene Peres <adriene_camargo@yahoo.com.br>
2024-01-12 15:50:21 -0300

    c2

--- Função decoradoras.py
+++ Função decoradoras.py
@@ -78,7 +78,7 @@
 
 def parametros_decorador(nome):
     def decorador(func):
-        print('Decorador: ', nome)
+        print('Decorador:', nome)
 
         def sua_nova_funcao(*args,**kwargs):
             res = func(*args,**kwargs)
@@ -87,6 +87,10 @@ def parametros_decorador(nome):
         return sua_nova_funcao
     return decorador
 
+@parametros_decorador(nome = 'quinto')
+@parametros_decorador(nome = 'quarto')
+@parametros_decorador(nome = 'terceiro')
+@parametros_decorador(nome = 'segundo')
 @parametros_decorador(nome = 'primeiro')
 def soma(x,y):
     return x+y
